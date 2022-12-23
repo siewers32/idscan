3267dbc0f312af96f9f5e68791fe78ba6d654f73
janjaap <janjaap@roc-dev.com>
2022-12-23 08:45:51 +0100

    betere jobtitles

--- database/factories/PersonFactory.php
+++ database/factories/PersonFactory.php
@@ -16,6 +16,7 @@ class PersonFactory extends Factory
      */
     public function definition()
     {
+        $jobtitles = $this->createJobTitles();
         $faker = Faker\Factory::create('nl_NL');
         $gender = (random_int(0,1))?'male':'female';
         return [
@@ -23,7 +24,32 @@ class PersonFactory extends Factory
             'firstname' => $faker->firstName($gender),
             'lastname' => $faker->lastName(),
             'email' => $faker->email(),
-            'jobtitle' => $faker->jobTitle(),
+            'jobtitle' => $faker->randomElement($jobtitles),
         ];
+    }
+
+    public function createJobTitles() {
+        $jobtitles = [
+            "projectleider" => 3,
+            "verkoopassistent" => 12,
+            "kassamedewerker" => 7,
+            "supervisor" => 4,
+            "medewerker M&O" => 5,
+            "kwaliteitsmanager" => 2,
+            "magazijnmedewerker" => 9,
+            "medewerker communicatie" => 5,
+            "directeur" => 1,
+            "hoofd administratie" => 1,
+            "medewerker administratie" => 22,
+            "commercieel directeur" => 1,
+            "medewerker salarisadministratie" => 2,
+            "verkoopadviseur" => 5
+        ];
+        foreach($jobtitles as $title => $number) {
+            for ($i = 0; $i < $number; $i++) {
+                $jts[] = $title;
+            }
+        }
+        return $jts;
     }
 }
